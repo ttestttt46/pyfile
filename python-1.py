e76ddc79df942207acf33162ea90be9cd2a774fc
ttestttt46 <ttestttt46@gmail.com>
2024-09-24 07:44:47 +0530

    Add files via upload

---
+++ python-1.py
@@ -0,0 +1,15 @@
+@app.route('/user/<path:user_id>')
+def get_user(user_id):
+conn = sqlite3.connect('users.db')
+cursor = conn.cursor()
+cursor.execute("SELECT name, email FROM users WHERE user_id = ?", (user_id,))
+data = cursor.fetchone()
+return render_template_string(f'''<!doctype html>
+<title>User information</title>
+10
+11
+12
+<h1>User</h1>
+<p>Name: {data[0]}</p>
+<p>Email: {data[1]}</p>
+</form>''')
